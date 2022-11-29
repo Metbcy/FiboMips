821969dd1cc728e1ee597ff58e6deab51da17809
Metbcy <Amirbredy1@gmail.com>
2022-11-29 12:24:21 -0500

    Update FiboMips.s

--- FiboMips.s
+++ FiboMips.s
@@ -1,10 +1,15 @@
 fib(int):
-    addiu $sp, $sp, -40
-    sw $31,36($sp)
-    sw $fp,32($sp)
-    sw $16,28($sp)
-    move $fp,$sp
-    sw $4,40($fp)
-    lw $2,40($fp)
+    addiu   $sp, $sp, -40
+    sw      $31,36($sp)
+    sw      $fp,32($sp)
+    sw      $16,28($sp)
+    move    $fp,$sp
+    sw      $4,40($fp)
+    lw      $2,40($fp)
     nop
-    beq $2,$0,$L2
+    beq     $2,$0,$L2
+    nop
+    beq     $2,$0,$L2
+    nop
+    lw      $3,40($fp)
+    li      $2,1                        # 0x1
